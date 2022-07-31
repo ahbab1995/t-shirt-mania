f505115fbad8bf6ecdabf02856d48fa4f70cab60
ahbab1995 <islamahbab1996@yahoo.com>
2022-08-01 00:23:29 +0600

    project on

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+        <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/home' element={<Home></Home>}></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>T-Shirt Mania</h1>
+            <nav>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
